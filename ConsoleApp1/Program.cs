8d62e7794744278dbfb0b269a83172d7cee941be
yourprettygood <79272017+yourprettygood@users.noreply.github.com>
2024-02-07 09:49:18 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            /*1 способ
+            int[] omas = new int[6]; // ЗАПОЛНЕНИЕ ОМ С КЛАВИАТУРЫ
+            for (int i = 0;i<6;i++)
+            {
+                Console.Write($"omas [{i}]=");
+                omas [i]= int.Parse(Console.ReadLine());
+            }
+            Console.ReadLine();
+            */
+            /* 2 способ
+            int[] omas = new int[6];
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.WriteLine($"omas[{i}]={omas[i]}");
+            }
+            */
+
+            //2) Инициализировать одномерный массив с использованием конструктора , без указания размерности массива и без клавиатуры. 
+            /*
+            int[] omas = { -3, 5, 7, -1, 10, 15 };
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.WriteLine($"omas[{i}]={omas[i]}");
+            }
+            */
+
+        }
+    }
+}
